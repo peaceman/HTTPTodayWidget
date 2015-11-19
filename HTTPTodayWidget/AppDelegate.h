1f295dc13bce089ae5119ec02d5a12f16acd606b
peaceman <nixalio@gmail.com>
2015-11-19 20:05:37 +0100

    Initial Commit

---
+++ HTTPTodayWidget/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HTTPTodayWidget
+//
+//  Created by Nico Nägele on 19/11/15.
+//  Copyright © 2015 n2305. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HTTPTodayWidget/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HTTPTodayWidget
+//
+//  Created by Nico Nägele on 19/11/15.
+//  Copyright © 2015 n2305. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
