c49047d5448737ca66bfec8337bec362cf2759a9
peaceman <nixalio@gmail.com>
2015-12-07 20:53:49 +0100

    add TriggerButton with NSLog action

---
+++ HTTP Widget/TodayViewController.h
@@ -0,0 +1,14 @@
+//
+//  TodayViewController.h
+//  HTTP Widget
+//
+//  Created by Nico Nägele on 19/11/15.
+//  Copyright © 2015 n2305. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *triggerButton;
+
+@end
---
+++ HTTP Widget/TodayViewController.m
@@ -0,0 +1,42 @@
+//
+//  TodayViewController.m
+//  HTTP Widget
+//
+//  Created by Nico Nägele on 19/11/15.
+//  Copyright © 2015 n2305. All rights reserved.
+//
+
+#import "TodayViewController.h"
+#import <NotificationCenter/NotificationCenter.h>
+
+@interface TodayViewController () <NCWidgetProviding>
+
+@end
+
+@implementation TodayViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
+    // Perform any setup necessary in order to update the view.
+    
+    // If an error is encountered, use NCUpdateResultFailed
+    // If there's no update required, use NCUpdateResultNoData
+    // If there's an update, use NCUpdateResultNewData
+
+    completionHandler(NCUpdateResultNewData);
+}
+
+- (IBAction)triggerAction:(UIButton*)sender {
+    NSLog(@"triggerButton triggered");
+}
+
+@end
